d0929322543d22ffe83dcbaec57bf89805dce134
Pavel Yakovlev <pavel_yakovlev2005@mail.ru>
2024-12-25 12:04:36 +0300

    Интерфейс + 3 модуль

--- src/main/java/com/cgvsu/model/Model.java
+++ src/main/java/com/cgvsu/model/Model.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class Model {
 
-    public static ArrayList<Vector3f> vertices = new ArrayList<>();
+    public ArrayList<Vector3f> vertices = new ArrayList<>();
     public ArrayList<Vector2f> textureVertices = new ArrayList<>();
     public ArrayList<Vector3f> normals = new ArrayList<>();
     public ArrayList<Polygon> polygons = new ArrayList<>();
@@ -24,29 +24,5 @@ public class Model {
 
     public boolean hasTexture() {
         return texture != null;
-    }
-
-    public static Vector3f scale = new Vector3f(1, 1, 1);
-    public static Vector3f rotation = new Vector3f(0, 0, 0);
-    public static Vector4f translation = new Vector4f(0, 0, 0, 1);
-
-    public static ArrayList<Vector3f> getVertices(){
-        return vertices;
-    }
-
-    public Vector3f getScale() {
-        return scale;
-    }
-
-    public void setScale(Vector3f scale) {
-        this.scale = scale;
-    }
-
-    public void setRotation(Vector3f rotation) {
-        this.rotation = rotation;
-    }
-
-    public void setTranslation(Vector4f translation) {
-        this.translation = translation;
     }
 }
