9ba38385031e2da2e2db0ba89f8003eb042109dd
Pavel Yakovlev <pavel_yakovlev2005@mail.ru>
2024-12-29 18:04:04 +0300

    Фикс бага с текстурой

--- src/main/java/com/cgvsu/rasterization/TexturedTriangleRenderer.java
+++ src/main/java/com/cgvsu/rasterization/TexturedTriangleRenderer.java
@@ -118,7 +118,7 @@ public class TexturedTriangleRenderer implements TriangleRenderer {
             int xMin = Math.max(0, x1);
             int xMax = Math.min(zBuffer.length - 1, x2);
 
-            for (int x = xMin; x <= xMax; x++) {
+            for (int x = xMin; x < xMax; x++) {
                 float z = Rasterization.interpolate(x, x1, x2, z1, z2);
                 if (z < zBuffer[x][y]) {
                     zBuffer[x][y] = z;
